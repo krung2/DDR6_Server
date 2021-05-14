7cf39edff393e15e4abe81ffd87fbbebd1bfe6bf
krung2 <jungbin4337@dgsw.hs.kr>
2021-05-14 21:07:56 +0900

    Add: class-validator & globalPipe & globalFilter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { PORT } from './config/dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ErrorFilter } from './fillters/error.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ErrorFilter());
   await app.listen(PORT);
 }
 bootstrap();
